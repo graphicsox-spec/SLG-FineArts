971d49f3bee4560cb706bcc64253c85092a1cf4d
RajputTalib <graphicsox@gmail.com>
2026-06-12 22:04:03 +0530

    updates

--- assets/js/components.js
+++ assets/js/components.js
@@ -96,7 +96,7 @@
         <div class="footer-brand">
           <img src="${r}assets/images/SLG-Signature-Logo-Footer.svg.svg" alt="SLG Masterworks" class="footer-brand-logo" />
         </div>
-        <p class="footer-tag">Fine art Wildlife, Landscape, Cityscape photography.<br>Limited-edition signed prints. Scottsdale, Arizona.<br>Shipped across North America<br>and Canada.</p>
+        <p class="footer-tag">Fine art Wildlife, Landscape, Water photography.<br>Limited-edition signed prints. Scottsdale, Arizona.<br>Shipped across North America<br>and Canada.</p>
         <div class="socials">
           <a href="https://www.instagram.com/" target="_blank" rel="noopener" aria-label="Instagram">
             <svg viewBox="0 0 24 24"><path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"/></svg>
@@ -128,7 +128,7 @@
         <ul>
           <li><a href="${r}the-images.html?collection=wildlife">Wildlife</a></li>
           <li><a href="${r}the-images.html?collection=landscape">Landscape</a></li>
-          <li><a href="${r}the-images.html?collection=cityscape">Cityscape</a></li>
+          <li><a href="${r}the-images.html?collection=water">Water</a></li>
           <li><a href="${r}shop.html?tag=new">New Arrivals</a></li>
           <li><a href="${r}shop.html?tag=best">Best Sellers</a></li>
         </ul>
